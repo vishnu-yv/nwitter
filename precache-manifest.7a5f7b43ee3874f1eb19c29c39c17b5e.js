75f8c7e0564dde18659fceeb53073a83ec6e1964
vishnu-yv <vishnubabu9876@gmail.com>
2020-10-06 16:58:11 +0530

    Updates

---
+++ precache-manifest.7a5f7b43ee3874f1eb19c29c39c17b5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f771949a2d7f258548d8034fe21955e4",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "ffdcca0a3efa1e37b57f",
+    "url": "/nwitter/static/css/main.9333dc4c.chunk.css"
+  },
+  {
+    "revision": "5ecd7351386e8178d771",
+    "url": "/nwitter/static/js/2.3747417b.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/nwitter/static/js/2.3747417b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffdcca0a3efa1e37b57f",
+    "url": "/nwitter/static/js/main.b23490b5.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
